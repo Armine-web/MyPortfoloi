cdd2a3933b0afd83187b5a583a59725ba2df76d2
Armine-web <83885299+Armine-web@users.noreply.github.com>
2022-07-17 02:35:41 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ $(function(){
     $(modal_resume).on("click", function() {
         $(modal_resume).removeClass('show');
 
-    }) ;
-
+    });
 });
+
+    
+    let noOfCharac = 0;
+    let contents = document.querySelectorAll(".content");
+    contents.forEach(content=>{
+        if(content.textContent.length < noOfCharac){
+            content.nextElementSibling.style.display="none";
+        }
+        else{
+          let displayText = content.textContent.slice(0, noOfCharac);
+          let moreText = content.textContent.slice(noOfCharac);
+          content.innerHTML = `${displayText}<span class="dots"></span><span class = "hide more">${moreText}</span>`;
+
+        }
+    });
+    
+    function readMore(btn){
+        let post = btn.parentElement;
+        post.querySelector(".dots").classList.toggle("hide");
+        post.querySelector(".more").classList.toggle("hide");
+        btn.textContent=="Read Less" ? btn.textContent="Read More": btn.textContent="Read Less";
+    }
+ 
